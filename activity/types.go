99236453df03835b7907bf031f7581fae2c83b14
Tai Kedzierski <dch.tai@gmail.com>
2026-03-24 09:44:50 +0000

    Make hours conditional

--- activity/types.go
+++ activity/types.go
@@ -37,9 +37,13 @@ type PrettyTime int
 func (pt PrettyTime) String() string {
 	secs := pt % 60
 	total_mins := (pt - secs) / 60
-	return fmt.Sprintf("%02d:%02d", total_mins, secs)
 
-	// mins := total_mins % 60
-	// hours := (total_mins - mins) / 60
-	// return fmt.Sprintf("%02d:%02d:%02d", hours, mins, secs)
+	if total_mins < 60 {
+		return fmt.Sprintf("%02d:%02d", total_mins, secs)
+	}
+
+	mins := total_mins % 60
+	hours := (total_mins - mins) / 60
+
+	return fmt.Sprintf("%02d:%02d:%02d", hours, mins, secs)
 }
